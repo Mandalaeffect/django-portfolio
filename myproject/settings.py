1f8baabb9687ceebc355351481ac62d6ab0c57bf
Mandalaeffect <138639978+Mandalaeffect@users.noreply.github.com>
2025-09-23 11:28:04 +0545

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lfe92_yx6o*xr$o04w%r(j_y0k%sjti5j^q6aor^bp$qhwc(%a'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-default-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['BibekRaya.pythonanywhere.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.pythonanywhere.com', '.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
